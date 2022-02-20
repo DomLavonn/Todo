90121cd3550e8245909d1b0c867f45ed44dec584
DomLavonn <99680793+DomLavonn@users.noreply.github.com>
2022-02-19 23:01:42 -0500

    done

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -4,19 +4,15 @@ import ToDoCard from "./components/UI/Card/ToDoCard";
 import Card from './components/UI/Card/Card'
 import TodoForm from "./components/Todo/TodoForm";
 import {useState} from "react";
+
 function App() {
     const [tdArr, setTdArr] = useState([])
-    let  todoList = [
-        {title: "Apply graduate School", subtitle: new Date()}
-    ]
 
-    const addToArrHandle = ()   => {
-
-    }
     const addTodoHandle = (tdItem) => {
 
-        todoList.push(tdItem)
-        console.log(todoList)
+        setTdArr( (prevState) => {
+            return [...prevState, {title: tdItem}]
+        })
     }
 
   return (
@@ -25,7 +21,7 @@ function App() {
         <Card title={"Enter todo item"}>
             <TodoForm addTodo={addTodoHandle} />
         </Card>
-        <ToDoCard card={todoList[0]}/>
+        {tdArr.length > 0 && <ToDoCard card={tdArr}/>}
     </div>
   );
 }
--- todolist/src/components/UI/Card/ToDoCard.js
+++ todolist/src/components/UI/Card/ToDoCard.js
@@ -3,15 +3,21 @@ import classes from './ToDoCard.module.css'
 import TodoButton from "../Button/TodoButton";
 
 const ToDoCard = (props) => {
-    return <Card className={classes.cardscreen}>
+
+    return<ul>
+
+     <Card className={classes.cardscreen}>
             <Card.Body>
-                <Card.Subtitle className="mb-2 text-muted">{props.card.subtitle.toTimeString()}</Card.Subtitle>
+                <Card.Subtitle className="mb-2 text-muted">{"Testing"}</Card.Subtitle>
                 <Card.Text>
-                    <p> {props.card.title} </p>
+                    {props.card.map(td => {
+                        return <p> {td.title} </p>
+                    })}
                 </Card.Text>
                 <TodoButton type={"submit"} value={"Submit"} />
             </Card.Body>
     </Card>
+    </ul>
 }
 
 export default ToDoCard
